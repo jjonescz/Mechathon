85bdc137567f776276d025fefca46d7e95e8842a
Jan Joneš <jjones@outlook.cz>
2020-02-29 14:09:52 +0100

    Disable BrickDetector

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from line_follower import LineFollower
 from brick_detector import BrickDetector
 
 if __name__ == "__main__":
+    print("Initializing")
+
     lf = LineFollower()
-    bd = BrickDetector()
+    #bd = BrickDetector()
 
     print("Started")
 
     while Button.DOWN not in brick.buttons():
-        sleep(lf.step())
-        bd.brickAhead()
+        lf.step()
+        # bd.brickAhead()
 
     print("Finished")
