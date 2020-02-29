5aca6e9a7891f3f7ccd52044e424f0e7a504e3e6
Jana Rezabkova <jana.rezabkova@gmail.com>
2020-02-29 12:21:53 +0100

    Detect brick on touch

--- brick_detector.py
+++ brick_detector.py
@@ -1,10 +1,20 @@
-import dependencies
+from dependencies import *
 
 
 class BrickDetector:
 
     def __init__(self):
         self.color = None
+        self.motor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+        self.color = ColorSensor(Port.S3)
+        self.touch = TouchSensor(Port.S4)
+        #self.ir = InfraredSensor(Port.S2)
+      
 
     def brickAhead(self):
-        return False
+        if touch.pressed():
+            motor.run_angle(10, 90)
+
+
+    def brickCollected(self):
+        motor.reset_angle(0)
